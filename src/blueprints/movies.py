0b7594237b62bf5e1e131882137ceb610a8950a7
Dimitris Athanasiadis <34347336+DimitrisAthanasiadis@users.noreply.github.com>
2022-03-06 22:25:21 +0200

    Get movies by rating

--- src/blueprints/movies.py
+++ src/blueprints/movies.py
@@ -17,7 +17,7 @@ def movie_by_category():
     category = request.get_json().get("category")
     movies = Movies.query.filter_by(movie_type=category).all()
 
-    return jsonify(movies=[movie.to_json() for movie in movies])
+    return jsonify(movies=[movie.to_json() for movie in movies]), 200
 
 
 @movies_bp.route('/movie_info', methods=['POST'])
@@ -25,4 +25,15 @@ def movie_info():
     movie_id = request.get_json().get("movie_id")
     movie = Movies.query.filter_by(id=movie_id).first()
 
-    return jsonify(movie=movie.to_json())
+    return jsonify(movie=movie.to_json()), 200
+
+
+@movies_bp.route("/rating", methods=['POST'])
+def movie_by_rating():
+    rating = request.get_json().get("rating")
+    if not rating:
+        return jsonify({"message": "Missing parameters"}), 400
+
+    movies = Movies.query.filter_by(rating=rating).all()
+
+    return jsonify(movies=[movie.to_json() for movie in movies]), 200
