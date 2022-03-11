e7861ff29112d6a051d2cb0da2240af22bdc82b8
Dimitris Athanasiadis <34347336+DimitrisAthanasiadis@users.noreply.github.com>
2022-03-12 00:12:39 +0200

    Added get rented movies, not returned movies and pay utility

--- src/blueprints/transaction.py
+++ src/blueprints/transaction.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, request
-from src.models import Movies, User
+from src.models import Movies, UserMovieRentals
 from src.utils.movie_utils import MoviesTransactions
 
 
@@ -71,6 +71,10 @@ def rent():
         return jsonify({"message": "Movie not found"}), 404
 
     movie_transactions = MoviesTransactions()
+
+    if movie_transactions.user_can_rent_movie(user_id=user_id, movie_id=movie_id) is False:
+        return jsonify({"message": "You can't rent this movie."}), 400
+
     transaction = movie_transactions.rent_transaction(
         movie=movie,
         days_to_rent=days_to_rent,
@@ -78,3 +82,85 @@ def rent():
     )
 
     return transaction
+
+
+@transaction_bp.route('/get_rented_movies', methods=['POST'])
+def get_rented_movies():
+    """
+    fetches the rented movies for the given user.
+
+    POST DATA:
+        user_id (int): the user id
+
+    Returns:
+        json: the jsonified list of rented movies
+    """
+
+    user_id = request.get_json().get("user_id")
+
+    if not user_id:
+        return jsonify({"message": "Missing parameters"}), 400
+
+    rented_movies = [movie.to_json() for movie in UserMovieRentals.query.filter_by(user_id=user_id).order_by(UserMovieRentals.rental_date.desc()).all()]
+
+    if not rented_movies:
+        return jsonify({"message": "No rented movies"}), 404
+
+    return jsonify(rented_movies), 200
+
+
+@transaction_bp.route("/get_not_returned_movies", methods=['POST'])
+def get_not_returned_movies():
+    """
+    fetches the not returned movies for the given user.
+
+    POST DATA:
+        user_id (int): the user id
+
+    Returns:
+        json: the jsonified list of not returned movies
+    """
+
+    user_id = request.get_json().get("user_id")
+
+    if not user_id:
+        return jsonify({"message": "Missing parameters"}), 400
+
+    not_returned_movies = [movie.to_json() for movie in UserMovieRentals.query.filter_by(user_id=user_id, returned=False).all()]
+    movies_transactions = MoviesTransactions()
+
+    for movie in not_returned_movies:
+        movie["extra_cost_penalty"] = movies_transactions.get_extra_cost_penalty(movie=movie)
+        movie["total_cost"] = movies_transactions.get_total_cost(movie=movie)
+
+    if not not_returned_movies:
+        return jsonify({"message": "All movies are returned and paid"}), 404
+
+    return jsonify(not_returned_movies), 200
+
+
+@transaction_bp.route('/pay', methods=['POST'])
+def pay():
+    """
+    performs the payment for the given movie.
+
+    POST DATA:
+        transaction_id (int): transaction id
+        user_id (int): user id
+        euros (float or int): the amount of euros to pay
+
+    Returns:
+        json: the jsonified answer about the payment
+    """
+
+    user_id = request.get_json().get("user_id")
+    transaction_id = request.get_json().get("transaction_id")
+    euros = request.get_json().get("euros")
+
+    if not user_id or not transaction_id or not euros:
+        return jsonify({"message": "Missing parameters"}), 400
+
+    movies_transactions = MoviesTransactions()
+    paid_movie = movies_transactions.pay(user_id=user_id, transaction_id=transaction_id, euros=euros)
+
+    return paid_movie
--- src/utils/movie_utils.py
+++ src/utils/movie_utils.py
@@ -72,3 +72,134 @@ class MoviesTransactions:
             "rent_price": self.rent_price(days_to_rent=days_to_rent),
             "rental_date": rental_date.strftime("%Y-%m-%d %H:%M:%S")
         }), 200
+
+    def get_extra_cost_penalty(self, **kwargs):
+        """
+        calculates the possible extra cost penalty
+        if the user hasn't returned the movie in time.
+
+        kwargs:
+            movie (Movie object): movie object
+
+        Returns:
+            int: depicts the extra cost penalty in euros.
+                the penalty is 2 euros per extra day.
+        """
+
+        from datetime import datetime, timedelta
+
+        movie = kwargs.get("movie")
+        extra_cost_penalty = 0
+
+        if movie.get("returned") is True:
+            return extra_cost_penalty
+
+        rental_date = datetime.strptime(movie.get("rental_date"), "%Y-%m-%d %H:%M:%S").date()
+        now = datetime.now().date()
+        supposed_return_date = rental_date + timedelta(days=movie.get("days_to_rent"))
+
+        if now < supposed_return_date:
+            return extra_cost_penalty
+
+        try:
+            day_difference = (now - supposed_return_date).days
+        except Exception as e:
+            print(f"EXCEPTION IN GET EXTRA COST PENALTY: {e}")
+            day_difference = 0
+
+        extra_cost_penalty = day_difference * 2
+
+        return extra_cost_penalty
+
+    def get_total_cost(self, **kwargs):
+        movie = kwargs.get("movie")
+        rent_price = self.rent_price(days_to_rent=movie.get("days_to_rent"))
+        extra_cost_penalty = self.get_extra_cost_penalty(movie=movie)
+
+        total_cost = rent_price + extra_cost_penalty
+
+        return total_cost
+
+    def user_can_rent_movie(self, **kwargs):
+        """
+        returns the ability of the user to rent a movie.
+
+        it depends on the existence of the movie in the
+        user_movie_rentals table. if the movie is absent from
+        the table or the user has returned the movie, the user
+        can rent the movie
+
+        kwargs:
+            movie_id (int): movie id
+            user_id (int): user id
+
+        Returns:
+            bool: the user can rent the movie or not
+        """
+
+        from src.models import UserMovieRentals
+
+        movie_id = kwargs.get("movie_id")
+        user_id = kwargs.get("user_id")
+
+        user_rented_movie = UserMovieRentals.query.filter_by(user_id=user_id, movie_id=movie_id).order_by(UserMovieRentals.rental_date.desc()).first()
+        if not user_rented_movie or user_rented_movie.returned is True:
+            return True
+        else:
+            return False
+
+    def pay(self, **kwargs):
+        """
+        performs the payment of the rental.
+
+        accepts movie id, user id and amount to pay.
+        checks if the user has rented the movie and
+        if the amount of money is enough to pay the
+        rented movie. the movie is then marked as
+        returned and the returned date is the current
+        datetime.
+
+        Returns:
+            json: the final jsonified answer of the payment
+        """
+
+        from src.models import UserMovieRentals
+        from datetime import datetime
+
+        transaction_id = kwargs.get("transaction_id")
+        user_id = kwargs.get("user_id")
+        euros = kwargs.get("euros")
+
+        user_rented_movie = UserMovieRentals.query.filter_by(user_id=user_id, id=transaction_id).first()
+
+        if not user_rented_movie or user_rented_movie.returned is True:
+            return jsonify({"message": "You haven't rented this movie or you have already paid for it in the last transaction."}), 400
+
+        total_cost = self.get_total_cost(movie=user_rented_movie.to_json())
+        if euros < total_cost:
+            return jsonify({
+                "message": f"You don't have enough money to pay for this movie. Total cost is {total_cost} euros",
+            }), 400
+        change = euros - total_cost
+
+        try:
+            user_rented_movie.returned = True
+            user_rented_movie.return_date = datetime.now()
+            db.session.commit()
+        except Exception as e:
+            print(e)
+            db.session.rollback()
+            return jsonify({
+                "message": "Error paying the movie",
+                "error": e
+            }), 500
+
+
+        return jsonify({
+            "message": "Movie paid successfully",
+            "movie_id": transaction_id,
+            "user_id": user_id,
+            "return_date": user_rented_movie.return_date.strftime("%Y-%m-%d %H:%M:%S"),
+            "returned": user_rented_movie.returned,
+            "change": change
+        }), 200
